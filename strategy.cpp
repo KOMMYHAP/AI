44d38ea641ac77383c991ae8fc27501d985c9f5c
Pavel <pasha.u.polyak@gmail.com>
2018-09-06 23:12:17 +0700

    Extended interface for strategies && added brief for RandomStrategy

--- strategy.cpp
+++ strategy.cpp
@@ -4,12 +4,17 @@
 
 namespace ai {
 
-model::Response RandomStrategy::getResponse() 
+model::Response RandomStrategy::getResponse()
 {
 	return {
 		model::Command{static_cast<model::Command::Type>(rand() % model::Command::Count)},
 		model::DebugMessage{"random command"}
 	};
+}
+
+bool RandomStrategy::isDone() const
+{
+	return false;
 }
 
 } // end of namespace ai
--- strategy.hpp
+++ strategy.hpp
@@ -12,21 +12,32 @@ namespace ai {
 		The same interface may be used for simple 
 		strategy like random choosing command or
 		complex like recurrent strategy which
-		allows to use details of old message. */
+		allows to use details of old message. 
+		
+		getResponse() returns response based on
+		implementation of strategy.
+
+		isDone() returns true if strategy completed
+		and false otherwise.
+		*/
 class Strategy
 {
 public:
 	virtual model::Response getResponse() = 0;
+	virtual bool isDone() const = 0;
 	virtual ~Strategy() = default;
 };
 
 using StrategyPtr = std::shared_ptr<Strategy>;
 
-/* Forward declarations of strategy implementation: */
+/*	@brief:
+		There is the easiest strategy and may be
+		base strategy for not null pointer */
 class RandomStrategy : public Strategy
 {
 public:
 	model::Response getResponse() override;
+	bool isDone() const override;
 	~RandomStrategy() override = default;	
 };
 
